ef1b8fe98e5758e8ade8bda17d272e1eb33efd62
Aakash Gupta <aakash.gupta96@yahoo.in>
2016-12-02 23:11:42 +0530

    Added authetication via phone no.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :bidded_projects, class_name: 'Project', :join_table => "bidders_projects",foreign_key: "bidder_id", association_foreign_key: "bidded_project_id"
   has_many :completed_projects, class_name: 'Project', foreign_key: "finisher_id" 
 
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where(["contact_no = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:contact_no) || conditions.has_key?(:email)
+        where(conditions.to_hash).first
+      end
+   end
+
 end
